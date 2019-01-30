941f6bf4e097a6b66e338edc20aca37e80908d33
Razvan Ardelean <37247858+ardeleanraz@users.noreply.github.com>
2019-01-30 16:26:52 +0200

    Add files via upload

---
+++ Scripturi/Follow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = player.position + offset;
+            
+    }
+}
---
+++ Scripturi/HitText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HitText : MonoBehaviour
+{
+    public Text hitText;
+    
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.name == "Obstacle")
+        {
+            hitText.text = "ai lovit ceva";
+        }
+
+    }
+
+
+}
+
